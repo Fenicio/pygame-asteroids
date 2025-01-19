18f40fd2c69845712b33ba8cb225032fcd7d553e
Guillermo G C <scorpid@gmail.com>
2025-01-19 09:27:44 +0100

    L2Ch5

--- main.py
+++ main.py
@@ -3,12 +3,14 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -16,7 +18,7 @@ def main():
 
         # clear the screen by filling it with black
         screen.fill((0, 0, 0))
-
+        player.draw(screen)
 
         pygame.display.flip()
         dt = clock.tick(60)
