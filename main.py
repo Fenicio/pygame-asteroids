3b2724e7b50ba75170ec5dabf303a596e9e62cb9
Fenicio <scorpid@gmail.com>
2025-01-19 09:02:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,14 +6,20 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        # clear the screen by filling it with black
         screen.fill((0, 0, 0))
 
+
         pygame.display.flip()
+        dt = clock.tick(60)
 
 if __name__ == "__main__":
     main()
